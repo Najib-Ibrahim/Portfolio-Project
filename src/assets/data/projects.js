c617737899035e826306c3e25a016f8cfac0c45a
Najib Ibrahim <najib98@protonmail.ch>
2023-03-23 21:16:07 +0000

    Update 23/03/2023

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -9,36 +9,31 @@ const projects = [
   {
     id: uuidv4(),
     name: 'Fridger',
-    desc:
-      'A fullstack application that creates a receipe based on the items in your fridge developed in React Native with a MongoDB Atlas cloud database',
+    desc: 'A fullstack application that creates a receipe based on the items in your fridge developed in React Native with a MongoDB Atlas cloud database',
     img: FridgerImg,
   },
   {
     id: uuidv4(),
     name: 'Tracker Sniffer',
-    desc:
-      'A web browser extension that exposes tracking algorithms used by websites visited by end user.',
+    desc: 'A web browser extension that exposes tracking algorithms used by websites visited by end user.',
     img: TrackerImg,
   },
   {
     id: uuidv4(),
     name: 'AI Sorting',
-    desc:
-      'An AI machine learning algorithm that efficiently and intelligently sorted through an extremely large data set.',
+    desc: 'An AI machine learning algorithm that efficiently and intelligently sorted through an extremely large data set.',
     img: ProjectImg,
   },
   {
     id: uuidv4(),
     name: 'Accessibility design',
-    desc:
-      'Wireframe design of web application with accessibility features in mind',
+    desc: 'Wireframe design of web application with accessibility features in mind',
     img: AccessImg,
   },
   {
     id: uuidv4(),
     name: 'Recipes',
-    desc:
-      'A mobile app that recommends popular recipes develpoed in Java and using an external API.',
+    desc: 'A mobile app that recommends popular recipes develpoed in Java and using an external API.',
     img: GreenCtgImg,
   },
 ];
--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -4,24 +4,21 @@ const testimonials = [
     name: 'Hanad Ahmed',
     title: 'Mobile App Dev',
     org: 'Williams Lee',
-    desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+    desc: 'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
     name: 'Abdulkadir Maxamed',
-    title: 'Founder',
+    title: 'Co-Founder',
     org: 'Pivot2Tech',
-    desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+    desc: 'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
   {
     id: 3,
     name: 'Yasin Hassan',
     title: 'Software Engineer',
     org: 'Capgemini',
-    desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+    desc: 'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
   },
 ];
 export default testimonials;
