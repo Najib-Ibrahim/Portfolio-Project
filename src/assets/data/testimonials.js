a11323f11f5ad86beed4aa93b77e87003b4975ca
Najib Ibrahim <najib98@protonmail.ch>
2023-03-26 23:20:17 +0100

    Testimonial update

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -4,21 +4,21 @@ const testimonials = [
     name: 'Hanad Ahmed',
     title: 'Mobile App Dev',
     org: 'Williams Lee',
-    desc: 'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+    desc: "I had the pleasure of working alongside Najib for several years, and I can honestly say that they are one of the most dedicated and talented professionals I've had the privilege to know. Their attention to detail and ability to manage complex projects was truly impressive, and they were always willing to go above and beyond to ensure that our team met our goals. Any organization would be lucky to have Najib on their team.",
   },
   {
     id: 2,
     name: 'Abdulkadir Maxamed',
     title: 'Co-Founder',
     org: 'Pivot2Tech',
-    desc: 'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+    desc: 'I had the pleasure of working with Najib on several projects, and I was always impressed by their ability to think creatively and solve problems in innovative ways. They have a keen eye for detail and are able to quickly identify areas for improvement, which made a real difference in the success of our projects. I would highly recommend Najib to any organization looking for a talented and driven professional.',
   },
   {
     id: 3,
     name: 'Yasin Hassan',
     title: 'Software Engineer',
     org: 'Capgemini',
-    desc: 'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+    desc: "I had the pleasure of working with Najib for several years, and I can honestly say that they are one of the most skilled communicators I've ever met. They have an incredible ability to connect with people from all backgrounds and cultures, and they were always able to build strong relationships with clients and stakeholders. Their expertise in project management and communication strategy was truly invaluable, and I would highly recommend them to anyone looking for a top-notch professional.",
   },
 ];
 export default testimonials;
